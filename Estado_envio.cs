da1ea7abb0f60af63a6c3c26ad86d28af5404cb3
Belen Manzano <belenmanzano16@gmail.com>
2019-09-06 23:50:30 -0400

    Pantallas principales

---
+++ Estado_envio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChileFast
+{
+    public partial class Estado_envio : Form
+    {
+        public Estado_envio()
+        {
+            InitializeComponent();
+        }
+    }
+}

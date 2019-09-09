6e98688199ab8fb97c32b07aecb4343daecad6f3
CRISTOBAL MAXIMILIANO CUBILLOS CACERES <420.cubillos@gmail.com>
2019-09-08 22:47:22 -0300

    Vista de valijas (se agrego formulario para agregar valijas)

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ChileFast
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Valijas());
         }
     }
 }
--- Valijas.cs
+++ Valijas.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,32 @@ namespace ChileFast
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+            conexion.Open();
+            string venta = txtVenta.Text;
+            string marca = txtMarca.Text;
+            string largo = txtLargo.Text;
+            string delicado = cmbDelicado.Text;
+            string peso = txtPeso.Text;
+            string altura = txtAltura.Text;
+            string ancho = txtAncho.Text;
+            string color = txtColor.Text;
+            string comentario = txtComentario.Text;
+
+            string cadena = "insert into Valijas(id,peso,altura,anchura,marca,color,largo,delicado,comentario)" +
+            "values ("+venta+","+ peso + "," + altura + "," + ancho + ",'"+marca+"','" + color + "',"+largo+",'"+delicado+"','" + comentario + "')";
+
+            SqlCommand comando = new SqlCommand(cadena, conexion);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Los datos se guardaron correctamente");
+            conexion.Close();
+            MessageBox.Show("Aviso de prueba : Se cerró la conexión.");
+        }
+
+        
     }
 }
